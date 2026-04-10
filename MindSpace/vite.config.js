5306a54490cc4962f4bda03fc232f359d7be52e9
JL Fuentes <methnavigator3@gmail.com>
2026-04-11 00:41:35 +0800

    Minorfixes

--- MindSpace/vite.config.js
+++ MindSpace/vite.config.js
@@ -3,17 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: './frontend',
-  build: {
-    outDir: '../dist',
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  base: '/', 
 })
